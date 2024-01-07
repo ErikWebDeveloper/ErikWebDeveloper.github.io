e8f8db6899a84245c0aaf90ccd1c5a6afe095257
Erik <erikcodedeveloper@gmail.com>
2024-01-07 21:50:58 +0100

    schedule

--- apps/marcar_hora/calendar.js
+++ apps/marcar_hora/calendar.js
@@ -400,7 +400,7 @@ var schedules = {
     years : 'all',
     months : [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     weeks : 'all',
-    days : [ 1, 2, 3, 4, 5]
+    days : [ 0, 1, 2, 3, 4, 5]
 }
 
 var calendar = new App(calendarConfig, schedules);
