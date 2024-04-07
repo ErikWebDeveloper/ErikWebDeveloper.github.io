ff9b4cd9d279a020f889c4ca7205254cff433659
ErikWebDeveloper <erikcodedeveloper@gmail.com>
2024-04-07 10:21:26 +0200

    update

---
+++ components/clipboard/snipet.js
@@ -0,0 +1,44 @@
+// Boton de ClipBoard
+const snipetCard = document.querySelectorAll("[clip-toogle]");
+
+// Icono de copiado exitoso
+const iconCopiedSuccess = `
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2-check" viewBox="0 0 16 16">
+                      <path d="M9.5 0a.5.5 0 0 1 .5.5.5.5 0 0 0 .5.5.5.5 0 0 1 .5.5V2a.5.5 0 0 1-.5.5h-5A.5.5 0 0 1 5 2v-.5a.5.5 0 0 1 .5-.5.5.5 0 0 0 .5-.5.5.5 0 0 1 .5-.5z"/>
+                      <path d="M3 2.5a.5.5 0 0 1 .5-.5H4a.5.5 0 0 0 0-1h-.5A1.5 1.5 0 0 0 2 2.5v12A1.5 1.5 0 0 0 3.5 16h9a1.5 1.5 0 0 0 1.5-1.5v-12A1.5 1.5 0 0 0 12.5 1H12a.5.5 0 0 0 0 1h.5a.5.5 0 0 1 .5.5v12a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5z"/>
+                      <path d="M10.854 7.854a.5.5 0 0 0-.708-.708L7.5 9.793 6.354 8.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/>
+                    </svg>
+        `;
+
+// Icono de copiado fallido
+const iconCopiedFailure = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-x" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M6.146 7.146a.5.5 0 0 1 .708 0L8 8.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 9l1.147 1.146a.5.5 0 0 1-.708.708L8 9.707l-1.146 1.147a.5.5 0 0 1-.708-.708L7.293 9 6.146 7.854a.5.5 0 0 1 0-.708"/>
+                  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1z"/>
+                  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0z"/>
+                </svg>
+        `;
+
+// Callback para boton de copiar
+function copyToClipBoard(event) {
+  let target =
+    event.target.tagName === "BUTTON" ? event.target : event.target.parentNode;
+  let btn = target;
+  let code = document.getElementById(
+    target.getAttribute("clip-toogle").split("#")[1]
+  ).textContent;
+
+  navigator.clipboard
+    .writeText(code)
+    .then(function () {
+      btn.innerHTML = iconCopiedSuccess;
+    })
+    .catch(function (error) {
+      btn.innerHTML = iconCopiedFailure;
+    });
+}
+
+// Listeners para boton copiar
+for (let i = 0; i < snipetCard.length; i++) {
+  snipetCard[i].addEventListener("click", copyToClipBoard);
+}
